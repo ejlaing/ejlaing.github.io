20ed990e3994f2f6631fe8c9eb3f5bd8582e93d1
ejlaing <elijlaing@gmail.com>
2022-02-07 18:22:39 -0600

    commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// import functions from https://www.gstatic.com/firebasejs/9.6.6/
+// Import functions from https://www.gstatic.com/firebasejs/9.6.6/
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
 import { 
   getFirestore, collection, getDocs,
-  doc, setDoc,
+  doc, setDoc, query, addDoc,
 } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore-lite.js';
 
-// initialize and configure Firebase
+// Initialize and configure Firebase
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyBlItp1Kq3TZp1aije7m9HxPzQG12zI7mU",
     authDomain: "fbla-attractions.firebaseapp.com",
@@ -15,10 +15,10 @@ const firebaseApp = initializeApp({
     appId: "1:1051181314735:web:2726c20e1841bb19a41a5a"
 });
 
-// initialize database
+// Initialize database
 const db = getFirestore(firebaseApp);
 
-// reference attractions collection
+// Reference attractions collection
 const attractions = collection(db, "attractions");
 
 // Write data to list
@@ -40,10 +40,12 @@ const q = query(collection(db, "attractions"));
 const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
   writeData(doc);
+  
 });
 
 // Add a new document in collection "attractions"
-function setNewDocument(newAttraction) {
+async function blahaba(newAttraction) {
+  console.log("coolio\n");
   const docRef = await addDoc(collection(db, "attractions"), {
     name: 	newAttraction.name,
     location: 	newAttraction.location,
@@ -53,5 +55,18 @@ function setNewDocument(newAttraction) {
     hours: 	newAttraction.hours,
     description: newAttraction.description,
   });
-  console.log("Document written with ID: " + docRef.id + " and\n name: " + docRef.doc.data);
+  //console.log("Document written with ID: " + docRef.id + " and\n name: " + docRef.doc.data());
 }
+
+// Get data from form and add a document with it
+var form = document.getElementById("add-doc-form");
+const attraction = {
+  name: "Andy's house",
+  location: "Fontana-On-Geneva-Lake",
+  zip: 53125,
+  rating: 5.0,
+  isRestaurant: false,
+  hours: {openTime: "12:00am", closeTime: "12:00pm"},
+  description: "Just a lovely home, innit!"
+}
+//blahaba(attraction);
